1a74cd05970a6721c2dca5a714fadc5e8c9c3232
Ana Laura Lara Chairez <97990917+Ana-Laura-Lara-Chairez@users.noreply.github.com>
2022-05-23 14:58:49 -0500

    Add files via upload

---
+++ PracticaDeJava.java
@@ -0,0 +1,32 @@
+
+package com.mycompany.practicaa;
+
+/**
+ *@author Ana Laura Lara Chairez
+ * Programa básico de comparación de 2 números
+ */
+public class PracticaDeJava {
+
+
+    public static void main(String[] args) {
+         int num1 = 5;
+         int num2 = 3;
+         
+         if (num1<num2){
+             System.out.println("El número 1 es menor al número 2");
+         }
+         else{
+             if (num1==num2){
+                 System.out.println("El número 1 es igual al número 2");
+             }
+             else{
+                    System.out.println("El número 1 es mayor al número 2");
+
+             }
+
+         }
+        
+        
+    }
+    
+}
